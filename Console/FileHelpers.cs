7ec48e1e2e588958bbad496fb9b4eacd6a2da6f7
Stefan Norinder <stefan.norinder@sh.se>
2022-10-08 14:30:19 +0200

    file operations

--- Console/FileHelpers.cs
+++ Console/FileHelpers.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 
 namespace Console
 {
@@ -6,9 +7,36 @@ namespace Console
     {
         public static string[] GetFilesRecursiveByName(string fileName, string path = "")
         {
-            if (string.IsNullOrEmpty(path)) path = Directory.GetCurrentDirectory();
-            
-            return  Directory.GetFiles(path,$"*{fileName}", SearchOption.AllDirectories);
+            if (string.IsNullOrEmpty(path)) path = CurrentDirectory;
+
+            return Directory.GetFiles(path, $"*{fileName}", SearchOption.AllDirectories);
         }
+
+        public static void CreateFile(string fileName, string relativePath, string content = "")
+        {
+            string path = $"{CurrentDirectory}/{relativePath}/{fileName}";
+            using (var fileStream = File.Create(path))
+            {
+                var  info = new UTF8Encoding(true).GetBytes(content);
+
+                fileStream.Write(info, 0, info.Length);
+            }
+        }
+
+        public static void DeleteFile(string fileName, string relativePath)
+        {
+            File.Delete($"{CurrentDirectory}/{relativePath}/{fileName}");
+        }
+
+        public static bool FileExists(string path)
+        {
+            return File.Exists($"{CurrentDirectory}/{path}");
+        }
+
+        #region private 
+
+        private static string CurrentDirectory => Directory.GetCurrentDirectory();
+        
+        #endregion
     }
 }
--- Tests/FileHelpersTests.cs
+++ Tests/FileHelpersTests.cs
@@ -9,6 +9,28 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
+            FileHelpers.CreateFile("inc.txt", "/TestData");
+        }
+        
+        [TearDown]
+        public void TearDown()
+        {
+            FileHelpers.DeleteFile("bar.txt", "/TestData");
+        }
+
+        [Test]
+        public void CreateFile()
+        {
+            FileHelpers.CreateFile("bar.txt", "/TestData");
+            Assert.IsTrue(FileHelpers.FileExists("/TestData/bar.txt"));
+        }
+
+        [Test]
+        public void RemoveFile()
+        {
+            if (!FileHelpers.FileExists("/TestData/inc.txt")) throw new System.Exception("File to remove does'n exist");
+            FileHelpers.DeleteFile("inc.txt", "/TestData");
+            Assert.IsFalse(FileHelpers.FileExists("/TestData/inc.txt"));
         }
 
         [Test]
@@ -16,6 +38,12 @@ namespace Tests
         {
             var files = FileHelpers.GetFilesRecursiveByName("foo.txt");
             Assert.AreEqual(1, files.Count());
+        }
+
+        [Test]
+        public void FileExists()
+        {
+            Assert.IsTrue(FileHelpers.FileExists("/TestData/foo.txt"));
         }
     }
 }
