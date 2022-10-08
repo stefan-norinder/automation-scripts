27d53821e6b68adb4bb97211134601e21022a7dd
Stefan Norinder <stefan.norinder@sh.se>
2022-10-08 14:57:32 +0200

    add row at position

--- Console/ContentHelpers.cs
+++ Console/ContentHelpers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Console
@@ -7,10 +8,31 @@ namespace Console
     {
         public static string AddRowFirst(string content, string row)
         {
-            var array = content.Split(Environment.NewLine);
-            var list = array.ToList();
-            list.Insert(0, row);
+            return InsertRowAtPosition(content, row, 0);
+        }
+
+        public static string AddRowLast(string content, string row)
+        {
+            return InsertRowAtPosition(content, row, NumberOfRows(content));
+        }
+
+        public static string InsertRowAtPosition(string content, string row, int position)
+        {
+            List<string> list = GetContentAsListOfRows(content);
+            list.Insert(position, row);
             return string.Join(Environment.NewLine, list);
-        }        
+        }
+
+        private static List<string> GetContentAsListOfRows(string content)
+        {
+            var array = content.Split(Environment.NewLine);
+            return array.ToList();
+        }
+
+        private static int NumberOfRows(string content)
+        {
+            var array = content.Split(Environment.NewLine);
+            return array.Count();
+        }
     }
 }
--- Tests/ContentHelpersTests.cs
+++ Tests/ContentHelpersTests.cs
@@ -25,6 +25,22 @@ namespace Tests
             Assert.AreEqual(expexted, result);
         }
 
-        
+        [Test]
+        public void AddRowLast()
+        {
+            var data = "This is the file foo \r\nAnd this is the second line \r\nThis is the third line";
+            var expexted = "This is the file foo \r\nAnd this is the second line \r\nThis is the third line\r\nI'm last";
+            var result = ContentHelpers.AddRowLast(data, "I'm last");
+            Assert.AreEqual(expexted, result);
+        }
+
+        [Test]
+        public void AddRowAtPosition()
+        {
+            var data = "This is the file foo \r\nAnd this is the second line \r\nThis is the third line";
+            var expexted = "This is the file foo \r\nHere I am\r\nAnd this is the second line \r\nThis is the third line";
+            var result = ContentHelpers.InsertRowAtPosition(data, "Here I am", 1);
+            Assert.AreEqual(expexted, result);
+        }
     }
 }
